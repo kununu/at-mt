2afb23ca969062eb8e3fc677e0a59487e7d4ffd9
Stefan Schacherl <stefan.schacherl@kununu.com>
2020-05-07 17:35:47 +0200

    It shows something

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Container from 'react-bootstrap/Container'
 import TestPlanForm from './components/TestPlan/TestPlanForm';
 import TestPlanList from './components/TestPlan/TestPlanList';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = ({}) => {
-
-  useEffect(() => {
-    // fetch data here
-    // use redux action to dispatch a fetch ...
-  }, []);
+const App = () => {
 
   return (
     <Container>
--- src/components/TestPlan/TestPlanForm.js
+++ src/components/TestPlan/TestPlanForm.js
@@ -7,103 +7,69 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 
 const TestPlanFormModal = () => {
-  const [state, setState] = useState({formItem: {}, show: true});
+  const [state, setState] = useState({item: {name:''}, show: true});
 
-  const onSubmit = () => setState();
   const handleClose = () => setState({
     ...state,
-    [state.show]: false
+    show: false
   });
+
+  const handleItemsChange = event => {
+    const value = event.target.value
+    setState({
+      ...state,
+      item: {
+        [event.target.name]: value
+      }
+    });
+  };
+
+  const submitItem = (event) => {
+    const item = {
+      name: state.item.name,
+      date: new Date().toISOString()
+    };
+
+    event.preventDefault();
+
+    axios
+      .post('http://localhost:3001/projects', item)
+      .then(response => {
+        console.log(response);
+      })
+
+    setState({
+      ...state,
+      show: false
+    });
+  }
 
   return (
     <Modal show={state.show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Add Test Plan </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form onSubmit={onSubmit}>
+      <Form onSubmit={submitItem}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Test Plan</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
           <Card.Title>Create New Test Plan</Card.Title>
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary">Close</Button>
-        <Button variant="primary">Save changes</Button>
-      </Modal.Footer>
+          <Form.Group controlId="formSuiteName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Test Plan Name"
+              name="name"
+              value={state.itemName}
+              onChange={handleItemsChange}
+            />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+          <Button variant="primary" type="submit">Save changes</Button>
+        </Modal.Footer>
+      </Form>
     </Modal>
   );
 };
-
-
-// const TestPlanFormModal = () => {
-//   const [show, setShow] = useState(true);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-//   const onSubmit = () => {
-//     setShow(false);
-//   };
-
-//   return (
-//     <Modal show={show} onHide={handleClose}>
-//       <Modal.Header closeButton>
-//         <Modal.Title>Add Test Plan</Modal.Title>
-//       </Modal.Header>
-//       <Modal.Body>
-//         <TestPlanForm onSubmit={onSubmit}></TestPlanForm>
-//       </Modal.Body>
-//     </Modal>
-//   );
-// }
-
-// const TestPlanForm = ({onSubmit, closeModal}) => {
-//   const [state, setState] = useState()
-
-//   const addItem = (event) => {
-//     event.preventDefault()
-//     const itemObject = {
-//       name: state,
-//       date: new Date().toISOString()
-//     };
-
-//     axios
-//       .post('http://localhost:3001/projects', itemObject)
-//       .then(response => {
-//         console.log(response)
-//       });
-//   }
-
-//   const handleItemsChange = (event) => {
-//     setState(event.target.value);
-//   }
-
-//   return (
-//     <Card>
-//       <Card.Header>Add Item</Card.Header>
-//       <Card.Body>
-//         <Form onSubmit={onSubmit}>
-//           <Card.Title>Create New Test Plan</Card.Title>
-//           <Form.Group controllId="formPlanName">
-//             <Form.Label>Name</Form.Label>
-//             <Form.Control
-//               type="text"
-//               placeholder="Test Plan Name"
-//               value={state}
-//               onChange={handleItemsChange}
-//             />
-//           </Form.Group>
-//           <div className="float-right">
-//             <Button variant="secondary" onClick={closeModal}>
-//               Close
-//             </Button>
-//             {' '}
-//             <Button variant="primary" type="submit">
-//               Submit
-//             </Button>
-//           </div>
-//         </Form>
-//       </Card.Body>
-//     </Card>
-//   )
-// }
 
 export default TestPlanFormModal;
