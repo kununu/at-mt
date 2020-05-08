f5a48c08eff190ffcae82730a6b74423cc4fad30
Stefan Schacherl <stefan.schacherl@kununu.com>
2020-05-08 15:10:45 +0200

    Add TicketsList

--- application/src/App.js
+++ application/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import ProjectsList from './components/Projects/ProjectsList';
-import TestCaseContainer from './components/TestCase/TestCasesContainer'
+import TestCaseContainer from './components/TestCase/TestCasesContainer';
+import TicketsList from './components/Tickets/TicketsList';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
     return (
-
       <Container>
         <ProjectsList />
+        <br />
+        <TicketsList />
         <br />
         <TestCaseContainer />
       </Container>
